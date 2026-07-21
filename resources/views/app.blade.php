034ad3f57226030a354a1e41e1fd6014109a4fb4
EUGENE OSEI SABIE <sabieeugeneosei@yahoo.com>
2026-07-21 20:14:56 +0000

    Google SEO config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="theme-color" content="#0f766e">
+        <meta name="google-site-verification" content="W7qPFeiw2kJx842pUU1u2DINljGp-fH83qfY6x8hvrM" />
 
         @php
             $siteName = config('app.name', 'Mummish');
